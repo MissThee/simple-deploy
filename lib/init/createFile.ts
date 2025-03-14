938b8fd914e670ba7bad4734f635a226f98952e3
MissThee <missthee@icloud.com>
2025-03-14 18:18:27 +0800

    feat: 调整创建的配置文件

--- lib/init/createFile.ts
+++ lib/init/createFile.ts
@@ -4,13 +4,19 @@ import {configFileExt} from "../../utils/global.js";
 // use json to create a file
 // json创建文件
 export default async (configFileFullPath: string, jsonObj: DeployConfig) => {
-    let jsonStr = JSON.stringify(jsonObj, null, '\t')
+    const jsonStr = JSON.stringify(jsonObj, null, '\t')
+    let res = ''
     switch (path.extname(configFileFullPath)) {
         case configFileExt.JS:
-            jsonStr= 'module.exports = ' + jsonStr
+            if (JSON.parse(fs.readFileSync('./package.json', 'utf8')).type === "module") {
+                res = 'export default ' + jsonStr
+            } else {
+                res = 'module.exports = ' + jsonStr
+            }
             break
         case configFileExt.JSON:
+            res = jsonStr
             break
     }
-    return await fs.promises.writeFile(configFileFullPath, jsonStr, 'utf8')
+    return await fs.promises.writeFile(configFileFullPath, res, 'utf8')
 }
